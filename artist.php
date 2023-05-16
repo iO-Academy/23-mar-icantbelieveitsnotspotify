af6bd5f131940c138e6e19d49e38212353d4e1fa
Henry Lee <hlee.0323@gmail.com>
2023-05-16 16:31:51 +0100

    (fix) added "http://" in artist.php header function

--- artist.php
+++ artist.php
@@ -3,7 +3,7 @@
 namespace Musicplayer;
 require "vendor/autoload.php";
 
-header('Access-Control-Allow-Origin: localhost:3000');
+header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Content-Type: application/json');
 
 use Musicplayer\Services\ArtistServices;
