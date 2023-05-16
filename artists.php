9228614bbe32a2533f33857d9eedcb9bf38913dc
James McGraw <james.mcgraw.allen@gmail.com>
2023-05-16 13:47:07 +0100

    (refactor) Added try catch for error codes, split format artists json response into 2 functions.
    Co-authored-by: Adam Young <adamyoungy678@gmail.com>
    Co-authored-by: James McGraw-Allen <james.mcgraw.allen@gmail.com>
    Co-authored-by: Henry Lee <hlee.0323@gmail.com>

--- artists.php
+++ artists.php
@@ -6,26 +6,19 @@ require "vendor/autoload.php";
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-//$artistsDao = new ArtistsDao();
-//
-//$artists = $artistsDao->fetchAll();
-//
-//$artistsJson = json_encode($artists);
-//
-//
-//echo $artistsJson;
-
-use Musicplayer\Database\ArtistDao;
 use Musicplayer\Services\ArtistsServices;
 
-//$artistsDao = new ArtistDao();
-//$artists = $artistsDao->fetchAllArtists();
-//
-//echo '<pre>';
-//print_r($artists);
-//echo '</pre>';
 
 $artistsServices = new ArtistsServices();
-$fetchedData = $artistsServices->formatArtistsJSONResponse();
 
-echo json_encode($fetchedData);
+try {
+    http_response_code(200);
+    $fetchedData = $artistsServices->formatArtistsJSONResponse();
+    echo json_encode($fetchedData);
+}
+catch (Exception $exception) {
+    http_response_code(500);
+    $data = json_encode(["message" => "Unexpected error", "data" => []]);
+    echo $data;
+    exit;
+}
