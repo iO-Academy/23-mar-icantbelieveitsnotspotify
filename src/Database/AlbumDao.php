61624d49e50036ef3756ea0cdc4bcd36b914778c
Adam Young <adamyoungy678@gmail.com>
2023-05-18 10:37:21 +0100

    (review) removed unused function Co-authored-by: Adam Young <adamyoungy678@gmail.com> Co-authored-by: Henry Lee <hlee.0323@gmail.com>

--- src/Database/AlbumDao.php
+++ src/Database/AlbumDao.php
@@ -27,21 +27,6 @@ class AlbumDao
         return new Album($album['id'], $album['album_name'], $album['artwork_url'], $album['artist_id']);
     }
 
-    public function fetchAlbumFromArtistId(int $artistId): Album
-    {
-        $sql = 'SELECT `id`, `album_name`, `artwork_url`, `artist_id` '
-            . 'FROM `albums`'
-            . 'WHERE `artist_id` = :id; ';
-
-        $value = [':id' => $artistId];
-
-        $query = $this->db->getPdo()->prepare($sql);
-        $query->execute($value);
-        $album = $query->fetch();
-
-        return new Album($album['id'], $album['album_name'], $album['artwork_url'], $album['artist_id']);
-    }
-
     public function fetchAllAlbumsFromArtistId(int $artistId): array
     {
         $sql = 'SELECT `id`, `album_name`, `artwork_url`, `artist_id` '
