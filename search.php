ebc084cda98127bae8f3981f4746cfa4e4d8953b
Dom <domlwheeler@outlook.com>
2023-05-18 16:17:15 +0100

    (progress) format better

--- search.php
+++ search.php
@@ -17,9 +17,26 @@ try {
         $searchResults = []; // Handle the case where $searchQuery is null
     } else {
         $searchResults = $songDao->getSearchResults($searchQuery);
+//        foreach ($searchResults as $result) {
+//            print_r($result['is_fav']);
+//                $result['is_fav'] = 8;
+//        }
+        for ($i = 0; $i < sizeof($searchResults); $i++) {
+
+            $searchResults[$i]['length'] = ltrim(date('i:s', round((floor($searchResults[$i]['length']) * 60) +
+                ($searchResults[$i]['length'] - floor($searchResults[$i]['length'])) * 100)), '0');
+
+            if ($searchResults[$i]['is_fav'] === 0) {
+                $searchResults[$i]['is_fav'] = false;
+            } else {
+                $searchResults[$i]['is_fav'] = true;
+            }
+        }
+
     }
 
     $data = json_encode($searchResults);
+
 } catch (\PDOException $exception) {
     http_response_code(500);
     $data = json_encode(["message" => "Unexpected error"]);
